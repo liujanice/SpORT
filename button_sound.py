d517c9e506712503d173087d76a07c4ec32e9c14
liujanice <janiceliu.2019@gmail.com>
2018-03-27 16:22:18 -0700

    add link to beep sound

--- button_sound.py
+++ button_sound.py
@@ -1,5 +1,5 @@
 from pygame import mixer # Load the required library
 
 mixer.init()
-mixer.music.load('link.mp3')
+mixer.music.load('SpORT/Grocery Scanning-SoundBible.com-1637675042.mp3')
 mixer.music.play()
