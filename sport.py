8669b62b1cd7a2569cd7451191ce91e097947a60
liujanice <janiceliu.2019@gmail.com>
2018-04-08 23:23:22 -0700

    update

--- sport.py
+++ sport.py
@@ -2,19 +2,12 @@
 #!/usr/bin/env python
 '''
 pip install pyglet
-python3 sport/pyglet-pyglet/setup.py install
+python3 sport/setup.py install
 you will also need FFmpeg.
-sport janicel$ python3 sport.py
+sport user$ python3 sport.py
 '''
 import pyglet
 song = pyglet.media.load('GroceryScanningSound.wav')
 song.play()
 pyglet.app.run()
-'''
-from pygame import mixer # Load the required library
 
-mixer.init()
-#mixer.music.load('GroceryScanning.mp3')
-mixer.music.load('GroceryScanningSound.wav')
-mixer.music.play()
-'''
