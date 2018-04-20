48daa47c89da07a5328f029e9f1fe82f294a7d3b
liujanice <janiceliu.2019@gmail.com>
2018-04-20 15:26:23 -0700

    updated code

--- sport.py
+++ sport.py
@@ -1,9 +1,3 @@
-#pwd to Documents working directory
-'''
-pip install pyglet
-python3 sport/setup.py install
-sport user$ python3 sport.py
-'''
 import pyglet
 song = pyglet.media.load('GroceryScanningSound.wav')
 song.play()
